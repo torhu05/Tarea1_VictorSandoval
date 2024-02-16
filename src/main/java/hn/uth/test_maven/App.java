2a2db34cd729899c634a9dc6b178587ff235a236
torhu05 <vhsandoval@uth.hn>
2024-02-15 22:25:19 -0600

    main

--- src/main/java/hn/uth/test_maven/App.java
+++ src/main/java/hn/uth/test_maven/App.java
@@ -1,5 +1,5 @@
 package hn.uth.test_maven;
-
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -7,6 +7,47 @@ package hn.uth.test_maven;
  */
 public class App 
 {
+	
+	public static void main( String[] args ){
+        System.out.println( "Tarea 1 Primer Parcial Victor Sandoval" );
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingrese el radio del cirulo");
+        double radio = scan.nextDouble();
+        System.out.println("Ingrese el lado del cuadrado");
+        double ladoCuadrado = scan.nextDouble();
+        System.out.println("Ingrese el lado 1 del Rectangulo");
+        double lado1Rectangulo = scan.nextDouble();
+        System.out.println("Ingrese el lado 2 del Rectangulo");
+        double lado2Rectangulo = scan.nextDouble();
+        System.out.println("Ingrese el lado 1 del Triangulo");
+        double lado1Triangulo = scan.nextDouble();
+        System.out.println("Ingrese el lado 2 del Triangulo");
+        double lado2Triangulo = scan.nextDouble();
+        
+        double areaCirculo, areaCuadrado, areaRectangulo, areaTriangulo;
+        areaCirculo = areaCirculo(radio);
+        areaCuadrado = areaCuadrado(ladoCuadrado);
+        areaRectangulo = areaRectangulo(lado1Rectangulo,lado2Rectangulo);
+        areaTriangulo = areaTriangulo(lado1Triangulo, lado2Triangulo);
+        
+        if(areaCirculo == -1) {
+            System.out.println("Area del circulo no definida"); 
+            }else
+            	System.out.println("El area del circulo es: " + areaCirculo);
+        if(areaCuadrado == -1) {
+            System.out.println("Area del Cuadrado no definida"); 
+            }else
+            	System.out.println("El area del Cuadrado es: " + areaCuadrado);
+        if(areaRectangulo == -1) {
+            System.out.println("Area del Rectangulo no definida"); 
+            }else
+            	System.out.println("El area del Rectangulo es: " + areaRectangulo);
+        if(areaTriangulo == -1) {
+            System.out.println("Area del Triangulo no definida"); 
+            }else
+            	System.out.println("El area del Triangulo es: " + areaTriangulo);
+    }
+	
 	public static double areaCirculo(double radio) {
     	if(noNegativo(radio)) {
     		return Math.pow(radio,2)*Math.PI;
